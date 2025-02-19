bb9b57ff55dc87a40d5dc665700faf7373e35b09
GregoriSimei <gregorisimei@gmail.com>
2025-02-18 22:24:16 -0300

    ci: build s3 before terraform init

--- main.tf
+++ main.tf
@@ -2,32 +2,12 @@ provider "aws" {
   region = "us-west-2"
 }
 
-data "aws_s3_bucket" "existing_bucket" {
-  bucket = "tech-challenge-tf-state"
-}
-
 terraform {
-  backend "s3" {}
-}
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "tech-challenge-tf-state"
-}
-
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "terraform-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
+  backend "s3" {
+    bucket         = "tech-challenge-tf-state"
+    key            = "terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
   }
 }
