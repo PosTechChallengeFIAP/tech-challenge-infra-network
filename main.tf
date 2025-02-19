7c2f904b19688d637a1332aab103f5507c7f1336
GregoriSimei <gregorisimei@gmail.com>
2025-02-18 21:48:46 -0300

    ci: add s3 to save tf configuration

--- main.tf
+++ main.tf
@@ -1,12 +1,33 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_s3_bucket" "existing_bucket" {
+  bucket = "tech-challenge-tf-state"
+}
+
 terraform {
-  required_provider {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.70"
-    }
+  backend "s3" {}
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "tech-challenge-tf-state"
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
-provider "aws" {
-  region = "us-west-2"
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
