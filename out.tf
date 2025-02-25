e81120edfa6e30e5681e0807dd74db759c179939
GregoriSimei <gregorisimei@gmail.com>
2025-02-24 23:56:22 -0300

    ci: expose auroras subnet group name

--- out.tf
+++ out.tf
@@ -14,3 +14,8 @@ output "aurora_security_group_id" {
   description = "ID do security group do Aurora"
   value       = aws_security_group.aurora_sg.id
 }
+
+output "aurora_subnet_group_name" {
+  description = "Subnet group name of Aurora"
+  value       = aws_db_subnet_group.aurora_subnet_group.name
+}
