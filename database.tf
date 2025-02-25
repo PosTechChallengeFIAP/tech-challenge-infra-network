e8cc04d0c82a366027c396a0f01cb159c10622db
GregoriSimei <gregorisimei@gmail.com>
2025-02-24 23:13:26 -0300

    fix: add 2 diferents regions

--- database.tf
+++ database.tf
@@ -1,6 +1,8 @@
 resource "aws_subnet" "private_aurora_a" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "tech-challenge-private-aurora-subnet-a"
@@ -8,8 +10,10 @@ resource "aws_subnet" "private_aurora_a" {
 }
 
 resource "aws_subnet" "private_aurora_b" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-west-2b"
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "tech-challenge-private-aurora-subnet-b"
