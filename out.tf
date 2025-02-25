4ce0f72db245b6a7fa02ea43e69ae7da122319b5
GregoriSimei <gregorisimei@gmail.com>
2025-02-24 22:56:35 -0300

    fix: change aurora sg reference

--- out.tf
+++ out.tf
@@ -9,3 +9,8 @@ output "public_subnet_id" {
 output "security_group_id" {
   value = aws_security_group.allow_all.id
 }
+
+output "aurora_security_group_id" {
+  description = "ID do security group do Aurora"
+  value       = aws_security_group.aurora_sg.id
+}
