e040563d38c98ee0dca1b9c25f182f46c3f23913
GregoriSimei <gregorisimei@gmail.com>
2025-03-04 18:20:42 -0300

    refactor: remove unused tf data

--- data.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "app" {
-  backend = "s3"
-  config = {
-    bucket = "tech-challenge-tf-state-bucket"
-    key    = "app/terraform.tfstate"
-    region = "us-west-2"
-  }
-}
