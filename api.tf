3deddbe9d5b18e5a7e7851f15f8e9cd97c250669
GregoriSimei <gregorisimei@gmail.com>
2025-03-22 01:29:17 -0300

    ci: allow protocols

--- api.tf
+++ api.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "api" {
   ingress {
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
